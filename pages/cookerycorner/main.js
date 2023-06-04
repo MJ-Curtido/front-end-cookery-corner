301411a3abae06bc2b12d9be8e8e1602f96fee70
Manu <manu17pc@gmail.com>
2023-06-04 11:24:58 +0200

    página principal hecha

--- pages/cookerycorner/main.js
+++ pages/cookerycorner/main.js
@@ -1,51 +1,116 @@
-import React, { useState } from 'react';
+//#region Imports
+import React, { useEffect, useState } from 'react';
 import AuthGuard from '@/components/additional/AuthGuard';
 import SearchBar from '@/components/common/SearchBar/SearchBar';
 import Layout from '@/components/additional/Layout';
 import ListRecipesCards from '@/components/common/ListRecipesCards/ListRecipesCards';
 import { recipesAvailable, searchRecipes } from '@/api/recipePetitions';
 import Cookies from 'universal-cookie';
+import { Pagination, Typography } from '@mui/material';
+//#endregion
 
-const Main = ({ recipes }) => {
+const Main = ({ recipes, pages }) => {
+    //#region Elements
     const cookies = new Cookies();
     const [recipesList, setRecipesList] = useState(recipes);
+    const [totalPages, setTotalPages] = useState(pages);
+    const [page, setPage] = useState(1);
+    const [searching, setSearching] = useState(false);
+    const [searchWords, setSearchWords] = useState('');
     const token = cookies.get('token');
+    //#endregion
 
-    const search = async (search) => {
-        if (search === '') {
-            const recipes = await recipesAvailable(1, token);
-            setRecipesList(recipes);
+    useEffect(() => {
+        onChangePage();
+    }, [page]);
+
+    //#region Functions
+    const onChangePage = async () => {
+        if (searching) {
+            const obj = await searchRecipes(page, searchWords, token);
+            setRecipesList(obj.recipes);
         } else {
-            const recipes = await searchRecipes(1, search, token);
-            setRecipesList(recipes);
+            const obj = await recipesAvailable(page, token);
+            setRecipesList(obj.recipes);
         }
     };
+
+    const paginationHandler = (event, value) => {
+        console.log('value', value);
+        setPage(value);
+    };
+
+    const search = async (search) => {
+        setSearchWords(search);
+
+        if (search === '') {
+            const obj = await recipesAvailable(page, token);
+            setRecipesList(obj.recipes);
+            setTotalPages(Math.ceil(obj.totalRecipes / 10));
+            setPage(1);
+            setSearching(false);
+        } else {
+            try {
+                const obj = await searchRecipes(page, search, token);
+                setRecipesList(obj.recipes);
+                setTotalPages(Math.ceil(obj.totalRecipes / 10));
+                setPage(1);
+                setSearching(true);
+            } catch (error) {
+                setRecipesList([]);
+                setTotalPages(1);
+                setPage(1);
+            }
+        }
+    };
+    //#endregion
 
     return (
         <AuthGuard>
             <Layout>
                 <SearchBar onSearch={search} />
 
-                <ListRecipesCards recipes={recipesList} />
+                {recipesList.length === 0 ? (
+                    <Typography variant="h4" style={{ textAlign: 'center', color: '#424242', marginBottom: '70px' }}>
+                        No recipes found.
+                    </Typography>
+                ) : (
+                    <ListRecipesCards recipes={recipesList} />
+                )}
+
+                <div style={CSS.paginationDiv}>
+                    <Pagination count={totalPages} color="primary" variant="outlined" shape="rounded" onChange={paginationHandler} />
+                </div>
             </Layout>
         </AuthGuard>
     );
 };
 
+const CSS = {
+    paginationDiv: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '70px',
+    },
+};
+
 export async function getServerSideProps({ req }) {
     try {
         const token = req.cookies.token;
-        const recipes = await recipesAvailable(1, token);
+        const obj = await recipesAvailable(1, token);
 
         return {
             props: {
-                recipes,
+                recipes: obj.recipes,
+                pages: Math.ceil(obj.totalRecipes / 10),
             },
         };
     } catch (error) {
         return {
             props: {
                 recipes: [],
+                pages: 1,
             },
         };
     }
