f38a69d1101771beab58876d43473edce8e27606
Manu <manu17pc@gmail.com>
2023-06-09 01:41:51 +0200

    Purchased recipes terminado

--- api/purchasePetitions.js
+++ api/purchasePetitions.js
@@ -24,3 +24,57 @@ export const getIsBought = async (idRecipe, token) => {
         }
     }
 };
+
+export const getPurchasedRecipes = async (page, token) => {
+    try {
+        const response = await fetch(`http://127.0.0.1:3007/purchases?page=${page}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw { message: errorData.error, status: response.status };
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        if (error.status) {
+            throw error;
+        } else {
+            throw { message: 'There is something wrong.', status: 500 };
+        }
+    }
+};
+
+export const searchPurchasedRecipes = async (page, search, token) => {
+    try {
+        const response = await fetch(`http://127.0.0.1:3007/purchases/search/${search}?page=${page}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw { message: errorData.error, status: response.status };
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        if (error.status) {
+            throw error;
+        } else {
+            throw { message: 'There is something wrong.', status: 500 };
+        }
+    }
+};
